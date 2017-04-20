5940aab00d4c8b2b7b5bf49b50f7d30018386a8c
Facundo <rossifacundo94@gmail.com>
2017-04-19 22:17:15 -0300

    Initial commit

---
+++ src/main/java/com/oktrueque/OktruequeApplication.java
@@ -0,0 +1,12 @@
+package com.oktrueque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktruequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktruequeApplication.class, args);
+	}
+}
