fe14b90ce78ce5a413a9910fe38939b835a4db75
Nico <riosc.nicolas@gmail.com>
2017-07-24 01:42:01 -0300

    profile.html empezando a poner thymeleaf

--- src/main/java/com/oktrueque/controller/ProfileController.java
+++ src/main/java/com/oktrueque/controller/ProfileController.java
@@ -1,10 +1,13 @@
 package com.oktrueque.controller;
 
+import com.oktrueque.model.Tag;
 import com.oktrueque.model.Trueque;
 import com.oktrueque.model.User;
+import com.oktrueque.model.UserTag;
 import com.oktrueque.service.TruequeService;
 import com.oktrueque.service.UserService;
 import com.oktrueque.service.UserServiceImpl;
+import com.oktrueque.service.UserTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,22 +25,27 @@ public class ProfileController {
 
     private UserService userService;
     private TruequeService truequeService;
+    private UserTagService userTagService;
 
     @Autowired
-    public ProfileController(UserServiceImpl userService, TruequeService truequeService){
+    public ProfileController(UserServiceImpl userService, TruequeService truequeService, UserTagService userTagService){
         this.userService = userService;
         this.truequeService = truequeService;
+        this.userTagService = userTagService;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/profile")
     public String getProfile(Principal principal, Model model){
         User user = userService.getUserByUsername(principal.getName());
         List<Trueque> trueques = truequeService.findByUserOffererIdOrUserDemandantId(user.getId(), user.getId());
+        List<UserTag> tags = userTagService.getUserTagByUserId(user.getId());
         model.addAttribute("user", user);
-        model.addAttribute("hasItems", user.getItems()!=null ? true : false);
-        model.addAttribute("user", user.getItems());
-        model.addAttribute("hasTrueques", trueques != null ? true : false);
+        model.addAttribute("hasItems", user.getItems().size() != 0 ? true : false);
+        model.addAttribute("items", user.getItems());
+        model.addAttribute("hasTrueques", trueques.size() != 0 ? true : false);
         model.addAttribute("trueques", trueques);
+        model.addAttribute("hasTags", tags.size() != 0 ? true : false);
+        model.addAttribute("tags", tags);
         return "profile";
     }
 }
