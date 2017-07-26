525d2d6fa3da973633c9422bea8ffe9ca1e77a40
Nico <riosc.nicolas@gmail.com>
2017-07-26 17:42:12 -0300

    Ready

--- src/main/java/com/oktrueque/OktruequeApplication.java
+++ src/main/java/com/oktrueque/OktruequeApplication.java
@@ -3,6 +3,8 @@ package com.oktrueque;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class OktruequeApplication {
 
