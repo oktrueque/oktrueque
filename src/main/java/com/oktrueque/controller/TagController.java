7e575f43813274e07bae089e2bffc8f82c589929
Tomás <tomas.peirotti@gmail.com>
2017-07-29 21:23:25 -0300

    Avances en el POST del tagController

--- src/main/java/com/oktrueque/controller/TagController.java
+++ src/main/java/com/oktrueque/controller/TagController.java
@@ -3,13 +3,13 @@ package com.oktrueque.controller;
 
 import com.oktrueque.model.Tag;
 import com.oktrueque.model.User;
-import com.oktrueque.model.UserTag;
+import com.oktrueque.model.UserTagId;
 import com.oktrueque.service.TagServiceImpl;
-import com.oktrueque.service.UserService;
+import com.oktrueque.service.UserServiceImpl;
+import com.oktrueque.service.UserTagServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -24,12 +24,15 @@ import java.util.List;
 public class TagController {
 
     private TagServiceImpl tagServiceImpl;
-    private UserService userService;
+    private UserServiceImpl userServiceImpl;
+    private UserTagServiceImpl userTagServiceImpl;
 
     @Autowired
-    public TagController(TagServiceImpl tagServiceImpl, UserService userService){
+    public TagController(TagServiceImpl tagServiceImpl, UserServiceImpl userServiceImpl, UserTagServiceImpl userTagService, UserTagServiceImpl userTagServiceImpl){
         this.tagServiceImpl = tagServiceImpl;
-        this.userService = userService;
+        this.userServiceImpl = userServiceImpl;
+        this.userTagServiceImpl = userTagServiceImpl;
+
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/profile/edit/tags") //AutoComplete
@@ -37,16 +40,16 @@ public class TagController {
         List<Tag> tags = tagServiceImpl.findAll();
         return new ResponseEntity<>(tags, HttpStatus.OK);
     }
-//    @RequestMapping(method = RequestMethod.GET, value = "/profile/edit/tags")
-//    public @ResponseBody List<Tag> getTags(){
-//        return tagServiceImpl.findAll();
-//    }
 
     @RequestMapping(method = RequestMethod.POST, value = "/profile/edit/tags") //para crear los intereses nuevos
-    public String setUserTags(Principal principal, @RequestBody UserTag userTag){
-        User user = userService.getUserByUsername(principal.getName());
+    public ResponseEntity<List<UserTagId>> setUserTagIds(@RequestBody UserTagId userTagId, Principal principal){
 
-        return "updateProfile";
+        User user = userServiceImpl.getUserByUsername(principal.getName());
+      //  userTagServiceImpl.setUserTagByUserId(user_id,List)
+
+
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
