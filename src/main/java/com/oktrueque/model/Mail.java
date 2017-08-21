108996ebd09b9a9252e3b99388e6e00abdfd7d70
Facundo Rossi <rossifacundo94@gmail.com>
2017-08-21 20:22:52 -0300

    Send Email to confirm with template ready

---
+++ src/main/java/com/oktrueque/model/Mail.java
@@ -0,0 +1,64 @@
+package com.oktrueque.model;
+
+import java.util.Map;
+
+/**
+ * Created by Facundo on 8/21/2017.
+ */
+public class Mail {
+
+    private String mailTo;
+
+    private String mailSubject;
+
+    private String mailContent;
+
+    private String contentType;
+
+    private Map< String, Object > model;
+
+    public Mail() {
+        contentType = "text/plain";
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getMailSubject() {
+        return mailSubject;
+    }
+
+    public void setMailSubject(String mailSubject) {
+        this.mailSubject = mailSubject;
+    }
+
+    public String getMailTo() {
+        return mailTo;
+    }
+
+    public void setMailTo(String mailTo) {
+        this.mailTo = mailTo;
+    }
+
+    public String getMailContent() {
+        return mailContent;
+    }
+
+    public void setMailContent(String mailContent) {
+        this.mailContent = mailContent;
+    }
+
+    public Map < String, Object > getModel() {
+        return model;
+    }
+
+    public void setModel(Map < String, Object > model) {
+        this.model = model;
+    }
+
+}
