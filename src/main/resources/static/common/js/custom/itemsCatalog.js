ff7afe5bf8347b48bb3b2620741112e873dc6fe6
Felipe Dalla Costa <dallacostafelipe@gmail.com>
2017-08-25 01:16:52 -0300

    mejoras visuales

---
+++ src/main/resources/static/common/js/custom/itemsCatalog.js
@@ -0,0 +1,14 @@
+/**
+ * Created by felipe on 24/08/17.
+ */
+$(document).ready(function() {
+    $('.cui-ecommerce--catalog--item').mouseenter(function(){
+        $(this).find('.cui-ecommerce-catalog--item--infobox').removeClass('slideOutDown hidden');
+        $(this).find('.cui-ecommerce-catalog--item--infobox').addClass('slideInUp');
+
+    });
+    $('.cui-ecommerce--catalog--item').mouseleave(function(){
+        $(this).find('.cui-ecommerce-catalog--item--infobox').removeClass('slideInUp');
+        $(this).find('.cui-ecommerce-catalog--item--infobox').addClass('slideOutDown');
+    });
+});
