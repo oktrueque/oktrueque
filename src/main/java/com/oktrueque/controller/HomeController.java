a3f2abfbf8f4a76ddbb0811d4f94a9b2beadfbea
Tomás <tomas.peirotti@gmail.com>
2017-09-20 20:40:16 -0300

    Falta validacion y esta todo

--- src/main/java/com/oktrueque/controller/HomeController.java
+++ src/main/java/com/oktrueque/controller/HomeController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @Controller
 public class HomeController {
@@ -36,25 +39,33 @@ public class HomeController {
 
     @RequestMapping(method= RequestMethod.POST, value = "/resetPassword")
     public ResponseEntity resetPassword(@RequestBody String datos){
+
         Email emailObject = new Email();
         String email = "", username = "";
+
+        Map<String,Object> model = new LinkedHashMap<>();
+        Random rand = new Random();
+
         if(datos.contains("@")){
             email = datos;
             User user = userService.getUserByEmailOrUsername(email, username);
-            user.setPassword(email.substring(0,email.indexOf("@"))+Math.random()*10000);
+            user.setPassword(email.substring(0,email.indexOf("@"))+rand.nextInt(1000));
             userService.updateUser(user);
             emailObject.setMailTo(email);
             emailObject.setMailSubject("OkTrueque - Reset password");
-            emailService.sendMail(emailObject,"passwordReset");
+            model.put("user",user);
+            emailObject.setModel(model);
+            emailService.sendMail(emailObject,"passwordReset.ftl");
         } else {
             username = datos;
             User user = userService.getUserByEmailOrUsername(email, username);
-            user.setPassword(username+Math.random()*10000);
+            user.setPassword(username+rand.nextInt(1000));
             userService.updateUser(user);
-            emailService.sendMail(emailObject,"passwordReset");
             emailObject.setMailTo(user.getEmail());
             emailObject.setMailSubject("OkTrueque - Reset password");
-            emailService.sendMail(emailObject,"passwordReset");
+            model.put("user",user);
+            emailObject.setModel(model);
+            emailService.sendMail(emailObject,"passwordReset.ftl");
         }
 
 
