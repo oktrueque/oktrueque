8c9279aaddf8c5382712b5d6709159ccbe4df93c
Nico <riosc.nicolas@gmail.com>
2017-11-20 16:52:59 -0300

    Fixes

--- src/main/java/com/oktrueque/service/ItemServiceImpl.java
+++ src/main/java/com/oktrueque/service/ItemServiceImpl.java
@@ -73,7 +73,7 @@ public class ItemServiceImpl implements ItemService {
         if (matcher.find()) {
             if((principal != null) && (matcher.group(2).equals(principal.getName()))){
                 map.put("items", itemRepository.findByUser_UsernameAndStatusIsNotInOrderByIdDesc(
-                        principal.getName(), new int[]{Constants.ITEM_STATUS_DELETED, Constants.ITEM_STATUS_BANNED}, pageable));
+                        principal.getName(), new int[]{Constants.ITEM_STATUS_DELETED, Constants.ITEM_STATUS_BANNED, Constants.ITEM_STATUS_EXCHANGED}, pageable));
                 map.put("loggedIn", true);
                 map.put("usernameSearch", true);
                 return map;
